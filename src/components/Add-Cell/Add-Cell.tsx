b95f40532eacb7d64fff10a89599ae66a1620840
incogNic0 <incognico.dev@gmail.com>
2022-04-13 13:01:39 -0500

    implement AddCell component

---
+++ src/components/Add-Cell/Add-Cell.tsx
@@ -0,0 +1,30 @@
+import './Add-Cell.css';
+import { useActions } from '../../hooks';
+
+interface AddCellProps {
+	nextCellId: string | null;
+}
+
+const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
+	const { insertCellBefore } = useActions();
+
+	return (
+		<div className="add-cell-wrapper">
+			<div className="add-cell-divider"></div>
+			<button
+				className="button is-medium"
+				onClick={() => insertCellBefore(nextCellId, 'code')}
+			>
+				+ Code
+			</button>
+			<button
+				className="button is-medium"
+				onClick={() => insertCellBefore(nextCellId, 'text')}
+			>
+				+ Text
+			</button>
+		</div>
+	);
+};
+
+export default AddCell;
--- src/components/Cell-Lists/Cell-List.tsx
+++ src/components/Cell-Lists/Cell-List.tsx
@@ -1,11 +1,26 @@
 import CellListItem from './Cell-List-Item';
 import { useTypedSelector } from '../../hooks/use-type-selector';
 
+import AddCell from '../Add-Cell/Add-Cell';
+import React from 'react';
+
 const CellList: React.FC = () => {
-  const renderedCells = useTypedSelector(({ cells: { order, data } }) => {
-    return order.map((id) => <CellListItem key={id} cell={data[id]} />);
-  });
-  return <div>{renderedCells}</div>;
+	const renderedCells = useTypedSelector(({ cells: { order, data } }) => {
+		return order.map((id, i) => {
+			return (
+				<React.Fragment key={id}>
+					<AddCell nextCellId={id} />
+					<CellListItem cell={data[id]} />
+				</React.Fragment>
+			);
+		});
+	});
+	return (
+		<div>
+			{renderedCells}
+			<AddCell nextCellId={null} />
+		</div>
+	);
 };
 
 export default CellList;
